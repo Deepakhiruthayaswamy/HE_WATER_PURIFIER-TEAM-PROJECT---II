5be61f79c9f66392500233a3bd2773b6a4550a66
Dastan <shashankstrshekhar001@gmail.com>
2024-08-19 15:28:35 +0530

    product and singleproduct page updated

---
+++ singleProduct.js
@@ -0,0 +1,32 @@
+
+const queryString = window.location.search;
+const urlparms = new URLSearchParams(queryString);
+
+const vname= urlparms.get('name');
+const vprice= urlparms.get('price');
+const vSKU= urlparms.get('SKU');
+const vcategory= urlparms.get('category');
+const vdescription= urlparms.get('description');
+const vimage= urlparms.get('image');
+const vreview = urlparms.get('review');
+const reviewerName = urlparms.get('reviewerName');
+const vratings = urlparms.get('ratings')
+const vnumber = urlparms.get('number');
+
+
+
+document.getElementById('CustomaddName').innerText=vname;
+document.getElementById('Customaddimg').setAttribute('src',vimage);
+document.getElementById('Customaddreviewimg').setAttribute('src',vimage);
+document.getElementById('CustomaddPrice').innerText=vprice;
+document.getElementById('Customadddesc').innerText=vdescription;
+document.getElementById('CustomaddSKU').innerText=vSKU;
+document.getElementById('CustomaddCategory').innerText=vcategory;
+document.getElementById('Customaddreviewername').innerText=reviewerName;
+document.getElementById('Customaddreview').innerText=vreview;
+document.getElementById('Customaddratingstar').innerHTML=vratings;
+document.getElementById('Customaddtotalratings').innerText=vnumber;
+
+const alertmessage = ()=>{
+    alert("Item purchased");
+}
